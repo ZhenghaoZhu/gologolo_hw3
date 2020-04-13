56a05c952aef6d93c768d57a2954ce84fc9579ef
Zhenghao Zhu <kevinzhuzhenghao@gmail.com>
2020-04-13 00:37:32 -0400

    Updated Logo Schema.

--- server/models/Logo.js
+++ server/models/Logo.js
@@ -4,13 +4,13 @@ var LogoSchema = new mongoose.Schema({
   id: String,
   text: String,
   color: String,
-  fontSize: { type: Number, min: 2, max: 144 },
+  fontSize: { type: Number, min: 2, max: 30},
   backgroundColor: String,
   borderColor: String,
-  borderRadius: { type: Number, min: 2, max: 144},
-  borderWidth: { type: Number, min: 2, max: 144},
-  padding: { type: Number, min: 2, max: 144},
-  margin: { type: Number, min: 2, max: 144},
+  borderRadius: { type: Number, min: 2, max: 60},
+  borderWidth: { type: Number, min: 2, max: 30},
+  padding: { type: Number, min: 2, max: 30},
+  margin: { type: Number, min: 2, max: 20},
   lastUpdate: { type: Date, default: Date.now },
   ms: String
 });
